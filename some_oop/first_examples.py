468e60fe4a27d607b7f119fb1749a9b4209caf73
PavelMuslimov <pavel.muslimov98@gmail.com>
2021-06-29 14:41:20 +0300

    some oop was updated

--- some_oop/first_examples.py
+++ some_oop/first_examples.py
@@ -44,3 +44,72 @@ class Bus1(Vehicle1):
 
 busWithCapacity = Bus1('School Volvo', 180, 12)
 print(busWithCapacity.seating_capacity())
+
+# OOP Exercise 5: Define property that should have the same value for every class instance
+# Define a class attribute”color” with a default value white. I.e., Every Vehicle should be white.
+class Vehicle:
+    color = 'white'
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(Vehicle):
+    pass
+
+class Car(Vehicle):
+    pass
+
+objBus5, objCar5 = Bus('School Volvo', 180, 12), Car('Audi Q5', 240, 18)
+print('Color: ', objBus5.color, 'Vehicle name: ', objBus5.name, 'Speed: ', objBus5.max_speed, 'Mileage: ', objBus5.mileage)
+print('Color: ', objCar5.color, 'Vehicle name: ', objCar5.name, 'Speed: ', objCar5.max_speed, 'Mileage: ', objCar5.mileage)
+
+# OOP Exercise 6: Class Inheritance
+
+# Create a Bus child class that inherits from the Vehicle class. 
+# The default fare charge of any vehicle is seating capacity * 100. 
+# If Vehicle is Bus instance, we need to add an extra 10% on full fare as a maintenance charge. 
+# So total fare for bus instance will become the final amount = total fare + 10% of the total fare.
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(Vehicle):
+    def fare(self):
+        amount = super().fare()
+        return amount * 1.1
+
+objBus6 = Bus('School Volvo', 12, 50)
+print("Total Bus fare is: ", objBus6.fare())
+
+# OOP Exercise 7: Determine which class a given Bus object belongs to (Check type of an object)
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+objBus7 = Bus("School Volvo", 12, 50)
+print(type(objBus7))
+
+# OOP Exercise 8: Determine if School_bus is also an instance of the Vehicle class
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+objBus8 = Bus("School Volvo", 12, 50)
+print(isinstance(objBus8, Vehicle))
